a284a6251bd9b891177e45a0674790d89c60d066
Firelord-sama <black-narcissus@live.cn>
2020-11-30 11:04:03 +0800

    相同的树

---
+++ src/same-tree/s1/Solution.java
@@ -0,0 +1,26 @@
+package same-tree.s1
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (null == p && null == q) {
+            return true;
+        }
+        if (null == p || null == q) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
